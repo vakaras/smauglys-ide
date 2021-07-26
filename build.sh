d08a490ffbeb80a1b10ae43af50168d24434c522
Vytautas Astrauskas <vastrauskas@gmail.com>
2021-07-26 11:06:24 +0200

    Try ensuring pip is installed.

--- build.sh
+++ build.sh
@@ -29,8 +29,10 @@ if [[ "$SHOULD_BUILD" == "yes" ]]; then
     curl https://www.python.org/ftp/python/3.8.10/python-3.8.10-embed-amd64.zip -Lo Python.zip
     mkdir -p ../VSCode-win32-${VSCODE_ARCH}/Python
     unzip Python.zip -d ../VSCode-win32-${VSCODE_ARCH}/Python
-    ../VSCode-win32-${VSCODE_ARCH}/Python/python.exe -m pip install --upgrade pip setuptools wheel
-    ../VSCode-win32-${VSCODE_ARCH}/Python/python.exe -m pip install pylint
+    export PYTHON=../VSCode-win32-${VSCODE_ARCH}/Python/python.exe
+    $PYTHON -m ensurepip --default-pip
+    $PYTHON -m pip install --upgrade pip setuptools wheel
+    $PYTHON -m pip install pylint
 
     # Download extensions.
     curl https://github.com/microsoft/vscode-python/releases/download/2021.7.1060902895/ms-python-release.vsix -Lo ms-python.zip
