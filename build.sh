0f63850d80c1f74a1498571df97f65021cb79304
Vytautas Astrauskas <vastrauskas@gmail.com>
2021-07-27 10:02:06 +0200

    Try installing pylint via installer.

--- build.sh
+++ build.sh
@@ -32,17 +32,17 @@ if [[ "$SHOULD_BUILD" == "yes" ]]; then
 #   export PYTHON=../VSCode-win32-${VSCODE_ARCH}/Python/python.exe
 #   $PYTHON -m pip install pylint
 
-    # Copy Python
-    curl https://www.python.org/ftp/python/3.8.10/python-3.8.10-embed-amd64.zip -Lo Python.zip
-    mkdir -p ../VSCode-win32-${VSCODE_ARCH}/Python
-    unzip Python.zip -d ../VSCode-win32-${VSCODE_ARCH}/Python
-    export PYTHON=../VSCode-win32-${VSCODE_ARCH}/Python/python.exe
+#   # Copy Python
+#   curl https://www.python.org/ftp/python/3.8.10/python-3.8.10-embed-amd64.zip -Lo Python.zip
+#   mkdir -p ../VSCode-win32-${VSCODE_ARCH}/Python
+#   unzip Python.zip -d ../VSCode-win32-${VSCODE_ARCH}/Python
+#   export PYTHON=../VSCode-win32-${VSCODE_ARCH}/Python/python.exe
 
-    # https://stackoverflow.com/questions/42666121/pip-with-embedded-python
-    sed -i '/^#.*import site/s/^#//' ../VSCode-win32-${VSCODE_ARCH}/Python/python*._pth
-    curl https://bootstrap.pypa.io/get-pip.py -Lo get-pip.py
-    $PYTHON get-pip.py
-    $PYTHON -m pip install pylint
+#   # https://stackoverflow.com/questions/42666121/pip-with-embedded-python
+#   sed -i '/^#.*import site/s/^#//' ../VSCode-win32-${VSCODE_ARCH}/Python/python*._pth
+#   curl https://bootstrap.pypa.io/get-pip.py -Lo get-pip.py
+#   $PYTHON get-pip.py
+#   $PYTHON -m pip install pylint
 
     # Download extensions.
     curl https://github.com/microsoft/vscode-python/releases/download/2021.7.1060902895/ms-python-release.vsix -Lo ms-python.zip
