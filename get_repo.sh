268df155c88b8738cb267ecf9d5f527329a0f05a
Vytautas Astrauskas <vastrauskas@gmail.com>
2021-09-04 18:34:33 +0200

    Set version to 1.59.1

--- get_repo.sh
+++ get_repo.sh
@@ -6,8 +6,8 @@ if [ "$INSIDER" == "1" ]; then
 else
 	UPDATE_INFO=$(curl https://update.code.visualstudio.com/api/update/darwin/stable/lol)
 fi
-export LATEST_MS_COMMIT=$(echo $UPDATE_INFO | jq -r '.version')
-export LATEST_MS_TAG=$(echo $UPDATE_INFO | jq -r '.name')
+export LATEST_MS_COMMIT=3866c3553be8b268c8a7f8c0482c0c0177aa8bfa
+export LATEST_MS_TAG=1.59.1
 echo "Got the latest MS tag: ${LATEST_MS_TAG} version: ${LATEST_MS_COMMIT}"
 
 if [ "$INSIDER" == "1" ]; then
